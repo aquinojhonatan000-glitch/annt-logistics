b510d51755ac7380f353ec14150573947fe565ee
aquinojhonatan000-glitch <aquinojhonatan000@gmail.com>
2026-07-11 01:19:06 -0500

    Actualizar page.jsx

--- src/app/productos/page.jsx
+++ src/app/productos/page.jsx
@@ -96,6 +96,7 @@ proveedor, descuentos ya vencidos y costos de importación.
 
 <p className="mt-2 font-normal">
 🚚 Entrega estimada: 6 a 15 días hábiles.
+ 📲 Atención al cliente: Si necesitamos confirmar algún detalle de tu pedido, nos comunicaremos contigo mediante WhatsApp.
 </p>
 
 </div>
