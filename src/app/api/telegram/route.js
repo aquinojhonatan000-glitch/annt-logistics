65c084df140fa45a993bb9a83909f09840134fe1
aquinojhonatan000-glitch <aquinojhonatan000@gmail.com>
2026-07-12 13:18:56 -0700

    Enviar pedidos completos a Telegram

--- src/app/api/telegram/route.js
+++ src/app/api/telegram/route.js
@@ -2,64 +2,130 @@ import { NextResponse } from "next/server";
 
 export async function POST(request) {
 
-  try {
+try {
 
-    const body = await request.json();
+const body = await request.json();
 
-    const mensaje = `
+
+const productos = body.productos?.map((producto)=>{
+
+return `
+🛍️ ${producto.nombre}
+Cantidad: ${producto.cantidad}
+${producto.talla ? `👕 Talla: ${producto.talla}` : ""}
+${producto.color ? `🎨 Color: ${producto.color}` : ""}
+`;
+
+}).join("\n");
+
+
+
+const mensaje = `
+
 🛒 NUEVO PEDIDO ANNT LOGISTICS
+
 
 👤 Cliente:
 ${body.nombre}
 
+
+🪪 DNI:
+${body.dni}
+
+
 📱 Teléfono:
 ${body.telefono}
+
 
 📍 Dirección:
 ${body.direccion}
 
+
+💳 Pago:
+${body.pago}
+
+
+📦 Productos:
+
+${productos}
+
+
 💰 Total:
 S/ ${body.total}
+
+
+📄 Comprobante:
+${body.comprobante}
+
+
 `;
 
-    const token = process.env.TELEGRAM_BOT_TOKEN;
-    const chatId = process.env.TELEGRAM_CHAT_ID;
 
 
-    const respuesta = await fetch(
-      `https://api.telegram.org/bot${token}/sendMessage`,
-      {
-        method:"POST",
-        headers:{
-          "Content-Type":"application/json"
-        },
-        body:JSON.stringify({
-          chat_id:chatId,
-          text:mensaje
-        })
-      }
-    );
+const token = process.env.TELEGRAM_BOT_TOKEN;
+
+const chatId = process.env.TELEGRAM_CHAT_ID;
 
 
-    const data = await respuesta.json();
+
+const respuesta = await fetch(
+
+`https://api.telegram.org/bot${token}/sendMessage`,
+
+{
+
+method:"POST",
+
+headers:{
+
+"Content-Type":"application/json"
+
+},
+
+body:JSON.stringify({
+
+chat_id:chatId,
+
+text:mensaje
+
+})
+
+}
+
+);
 
 
-    return NextResponse.json(data);
+
+const data = await respuesta.json();
 
 
-  } catch(error){
 
-    console.log(error);
+return NextResponse.json(data);
 
-    return NextResponse.json(
-      {
-        error:"Error Telegram"
-      },
-      {
-        status:500
-      }
-    );
 
-  }
+}catch(error){
+
+
+console.log(error);
+
+
+return NextResponse.json(
+
+{
+
+error:"Error Telegram"
+
+},
+
+{
+
+status:500
+
+}
+
+);
+
+
+}
 
 }
