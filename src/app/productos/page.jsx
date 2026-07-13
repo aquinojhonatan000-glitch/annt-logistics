23838b2df8c94d97b49d79ba44e71414b4e2beb9
aquinojhonatan000-glitch <aquinojhonatan000@gmail.com>
2026-07-13 10:56:11 -0700

    visor imagen ampliada sin flecha

--- src/app/productos/page.jsx
+++ src/app/productos/page.jsx
@@ -597,34 +597,6 @@ rounded-xl
 />
 
 
-<button
-
-onClick={(e)=>{
-
-e.stopPropagation();
-
-setIndiceImagen(
-(indiceImagen + 1) % imagenActual.length
-);
-
-}}
-
-className="
-absolute
-right-10
-bg-white
-text-black
-px-4
-py-2
-rounded-full
-"
-
->
-
-➡️
-
-</button>
-
 
 </div>
 
